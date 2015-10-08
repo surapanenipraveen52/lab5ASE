509666d2d83fa6ac5f3fd547fe7c6d935f0e83cd
surapanenipraveen52 <surapanenipraveen@gmail.com>
2015-10-08 17:59:38 -0500

    Vid

--- Source/Doctor App/www/js/controllers.js
+++ Source/Doctor App/www/js/controllers.js
@@ -1,26 +1,16 @@
 angular.module('directory.controllers', ['ngCordova'])
 
-    .controller('EmployeeIndexCtrl', function ($scope, EmployeeService,$cordovaCamera) {
+    .controller('EmployeeIndexCtrl', function ($scope, EmployeeService,$cordovaCapture) {
 
         $scope.searchKey = "";
-       $scope.takePicture = function() {
-        var options = { 
-            quality : 75, 
-            destinationType : Camera.DestinationType.DATA_URL, 
-            sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
-           
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-        });
-    }
+       $scope.captureVideo = function() {
+    var options = { limit: 3, duration: 15 };
+
+        $cordovaCapture.captureVideo(options).then(function(videoData) {
+      alert(videoData);
+    }, function(err) {
+    });
+  }
 
         $scope.clearSearch = function () {
             $scope.searchKey = "";
